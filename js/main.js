7094aa62bb6389fe1253492ed4b705049efbd0cb
Jose Fuilco <jfuilcogal@gmail.com>
2023-04-03 22:02:34 -0500

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display")
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == "clear") {
+            display.innerText = ""
+        } else if(item.id == "back"){
+            let word = String(display.innerText)
+            display.innerText = word.substring(0, word.length - 1)
+        } else if (display.innerText != "" && item.id == "equal"){
+            display.innerText = parseFloat(eval(String(display.innerText))).toFixed(3)
+        } else if(display.innerText == "" && item.id == "equal"){
+            display.innerText = "Null"
+            setTimeout(()=>{display.innerText = ""}, 500)
+        } else {
+            display.innerText += String(item.id)
+        }
+    }
+})
